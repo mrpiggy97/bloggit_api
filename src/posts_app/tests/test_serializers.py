8ba42533dbb82d36a1d470cd522e06adc2fc006e
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-06-28 22:18:26 -0400

    created test for reading data revieved from PostSerializer and create Test class for CommentSerializer

--- src/posts_app/tests/test_serializers.py
+++ src/posts_app/tests/test_serializers.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 from rest_framework.test import APITestCase
 
-from posts_app.models import Post
-from posts_app.serializers import PostSerializer
+from posts_app.models import Post, CommentFeed, Comment
+from posts_app.serializers import PostSerializer, CommentSerializer
 
 from users_app.models import Sub
 
@@ -36,6 +36,37 @@ class TestPostSerializer(APITestCase):
         self.post.communities.add("test")
 
         self.serializer = PostSerializer
+
+    def test_read_post(self):
+        #first a post with its data serialized needs
+        #to be created
+
+        new_post = Post.objects.create(
+            title="this is the post to read",
+            text="this is the post to read",
+            owner=self.sub
+        )
+
+        new_post.communities.add("test")
+
+        serializer = self.serializer(new_post)
+
+        #data from serializer should be equal to this
+        expected_data = {
+            'uuid': str(new_post.uuid),
+            'title': new_post.title,
+            'text': new_post.text,
+            'communities_list': new_post.get_communities_as_list,
+            'liked': None,
+            'reported': None,
+            'likes': new_post.likes,
+            'reports': new_post.reports,
+            'owner': new_post.get_owner_info,
+            'pic': new_post.get_pic,
+            'date': new_post.get_date_posted
+        }
+
+        self.assertEqual(serializer.data, expected_data)
     
     def test_create_post(self):
         data = {
@@ -108,35 +139,120 @@ class TestPostSerializer(APITestCase):
 
         #check that update method does not return an instance object
         self.assertIsNone(serializer.update(self.post, should_not_work))
-    
 
-    def test_read_post(self):
-        #first a post with its data serialized needs
-        #to be created
 
-        new_post = Post.objects.create(
-            title="this is the post to read",
-            text="this is the post to read",
-            owner=self.sub
+class TestCommentSerializer(APITestCase):
+    '''test read create and update for CommentSerializer'''
+
+    def setUp(self):
+
+        self.user = User.objects.create_user(
+            username=user_test_data['username'],
+            password=user_test_data['password'],
+            email=user_test_data['email']   
         )
 
-        new_post.communities.add("test")
+        self.sub = Sub.objects.create(user=self.user)
 
-        serializer = self.serializer(new_post)
+        self.post = Post.objects.create(
+            title="testing comment",
+            text="testing comment serializer",
+            owner=self.sub
+        )
+        self.post.communities.add("test")
 
-        #data from serializer should be equal to this
+        self.commentfeed = CommentFeed.objects.create(post=self.post)
+
+        self.comment = Comment.objects.create(
+            text='this is the first comment',
+            owner=self.sub,
+            commentfeed=self.commentfeed
+        )
+
+        self.serializer = CommentSerializer
+    
+    def test_read_comment(self):
+        #check that data recieved from serializer is exactly as expeted
         expected_data = {
-            'uuid': str(new_post.uuid),
-            'title': new_post.title,
-            'text': new_post.text,
-            'communities_list': new_post.get_communities_as_list,
+            'owner': self.comment.get_owner_info,
+            'text': self.comment.text,
+            'uuid': self.comment.get_uuid_as_string,
+            'date': self.comment.get_date_posted,
+            'is_original': True,
+            'has_parent': False,
+            'pic': None,
+            'parent_comment': None,
+            'likes': self.comment.likes,
+            'reports': self.comment.reports,
             'liked': None,
-            'reported': None,
-            'likes': new_post.likes,
-            'reports': new_post.reports,
-            'username': new_post.get_username,
-            'pic': new_post.get_pic,
-            'date': new_post.get_date_posted
+            'reported': None
         }
 
+        serializer = self.serializer(self.comment)
+
         self.assertEqual(serializer.data, expected_data)
+    
+    def test_create_comment(self):
+        data = {
+            'text': 'this is a comment test',
+            'owner_uuid': str(self.sub.uuid),
+            'commentfeed_uuid': str(self.commentfeed.uuid),
+        }
+
+        serializer = self.serializer(data=data)
+
+        #first check if serializer is valid as how it is
+        self.assertTrue(serializer.is_valid())
+
+        if serializer.is_valid():
+            serializer.save()
+        
+        test_comment = Comment.objects.last()
+        test_serializer = CommentSerializer(test_comment)
+
+        self.assertEqual(test_serializer.data, serializer.data)
+    
+
+    def test_update_comment_with_correct_data(self):
+
+        #likes and reports are optional fields providing them is not nessesary
+        #has_parent and is_original fields don't do anything when updating
+        #providing them is not nessesary
+        expected_data = {
+            'text': 'updated comment text',
+            'likes': 100,
+            'reports': 100,
+            'owner_uuid': str(self.comment.owner.uuid),
+            'commentfeed_uuid': str(self.commentfeed.uuid)
+        }
+
+        serializer = self.serializer(self.comment, data=expected_data)
+        self.assertTrue(serializer.is_valid())
+
+        if serializer.is_valid():
+            serializer.save()
+
+        test_comment = Comment.objects.first()
+        test_serializer = CommentSerializer(test_comment)
+
+        self.assertEqual(test_serializer.data, serializer.data)
+        self.assertEqual(test_comment.likes, 100)
+    
+    def test_update_with_wrong_data(self):
+        #create a new commentfeed (or a sub it doesn't matter) and try to
+        #use it uuid to update the comment, this should not work
+
+        new_commentfeed = CommentFeed.objects.create(post=self.post)
+
+        should_not_work = {
+            'text': 'should not work',
+            'owner_uuid': str(self.sub.uuid),
+            'commentfeed_uuid': str(new_commentfeed.uuid),
+            'likes': 33 #not necessary to include
+        }
+
+        serializer = self.serializer(self.comment)
+
+        #commentfeed_uuid is not the same as the objects original
+        #commentfeed.uuid so .update() should return None
+        self.assertIsNone(serializer.update(self.comment, should_not_work))
