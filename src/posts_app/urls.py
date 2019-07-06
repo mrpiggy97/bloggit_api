51a19e222833e7d067ad313b33eca4b0b2511b71
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-07-06 19:37:25 -0400

    created urls make-post and delete-post

--- src/posts_app/urls.py
+++ src/posts_app/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('get-post/<str:post_uuid>/', PostView.as_view(), name='get_post'),
     #edit-post url
     path('edit-post/<str:post_uuid>/', PostView.as_view(), name='edit_post'),
+    #make-post url
+    path('make-post/', PostView.as_view(), name='make_post'),
+    #delete-post url
+    path('delete-post/<str:post_uuid>/', PostView.as_view(), name='delete_post'),
 ]
