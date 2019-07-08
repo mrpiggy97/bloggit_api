3fd21e99828f975ac78c5875077bd1e5b2f63f4d
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-07-08 00:01:37 -0400

    added new url  posts-by-community

--- src/posts_app/urls.py
+++ src/posts_app/urls.py
@@ -2,6 +2,7 @@
 
 from django.urls import path
 from .views.PostView import PostView
+from .views.PostsByCommunity import PostsByCommunity
 
 app_name = 'posts_app'
 urlpatterns = [
@@ -13,4 +14,7 @@ urlpatterns = [
     path('make-post/', PostView.as_view(), name='make_post'),
     #delete-post url
     path('delete-post/<str:post_uuid>/', PostView.as_view(), name='delete_post'),
+    #posts-by-community url
+    path('posts-by-community/<slug:community_slug>/', PostsByCommunity.as_view(),
+                                                        name='posts_by_community')
 ]
