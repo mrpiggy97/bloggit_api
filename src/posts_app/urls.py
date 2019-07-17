e11a4a869f5b6c5921e1ef3e7b58abc6f9d6cfec
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-07-17 15:32:07 -0400

    created new paths

--- src/posts_app/urls.py
+++ src/posts_app/urls.py
@@ -3,6 +3,7 @@
 from django.urls import path
 from .views.PostView import PostView
 from .views.PostsByCommunity import PostsByCommunity
+from .views.CommentView import CommentView
 
 app_name = 'posts_app'
 urlpatterns = [
@@ -16,5 +17,13 @@ urlpatterns = [
     path('delete-post/<str:post_uuid>/', PostView.as_view(), name='delete_post'),
     #posts-by-community url
     path('posts-by-community/<slug:community_slug>/', PostsByCommunity.as_view(),
-                                                        name='posts_by_community')
+                                                        name='posts_by_community'),
+    #get-comment url
+    path('get-comment/<uuid:comment_uuid>/', CommentView.as_view(), name='get_comment'),
+    #edit-comment url
+    path('edit-comment/<uuid:comment_uuid>/',  CommentView.as_view(), name='edit_comment'),
+    #make-comment url
+    path('make-comment/', CommentView.as_view(), name='make_comment'),
+    #delete-comment url
+    path('delete-comment/<uuid:comment_uuid>/', CommentView.as_view(), name='delete_comment'),
 ]
