86f42344c5eaf581ec766809270b3740f24bfef7
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-08-10 23:24:17 -0400

    created new test for newly created view

---
+++ src/posts_app/tests/test_endpoints/test_home.py
@@ -0,0 +1,24 @@
+#test home endpoint
+
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
+
+from posts_app.tests.utils import create_post, create_sub, create_user
+
+class TestHomeView(APITestCase):
+    
+    def setUp(self):
+        
+        self.user = create_user()
+        self.sub = create_sub(self.user)
+        self.path = '/posts/?page=20'
+        self.client = APIClient()
+        
+        for n in range(0, 50):
+            create_post(self.sub)
+    
+    def test_success_response(self):
+        
+        response = self.client.get(path=self.path)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
