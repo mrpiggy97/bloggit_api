d126dd1cd1b8cd7cca1a31d57f351c5fbaf2a718
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-08-25 21:57:20 -0400

    added new paths

--- src/users_app/urls.py
+++ src/users_app/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 from .views.CommunitiesFollowed import CommunitiesFollowed
 from .views.ProfileData  import ProfileData
+from .views.LikesReports import like_post, like_comment, report_post, report_comment
 
 app_name = 'users_app'
 
@@ -15,4 +16,12 @@ urlpatterns = [
     path('unsubscribe/<slug:community_slug>', CommunitiesFollowed.as_view(),
                                                 name='unsubscribe'),
     path('profile/<uuid:sub_uuid>/', ProfileData.as_view(), name='profile'),
+    #like-post url
+    path('like-post/<uuid:post_uuid>/', like_post, name='like_post'),
+    #like-comment url
+    path('like-comment/<uuid:comment_uuid>/', like_comment, name='like_comment'),
+    #report-post url
+    path('report-post/<uuid:post_uuid>/', report_post, name='report_post'),
+    #report-comment url
+    path('report-comment/<uuid:comment_uuid>/', report_comment, name='report_comment'),
 ]
