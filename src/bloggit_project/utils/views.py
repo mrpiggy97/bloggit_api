695d5ff76118ebebe3abecd990bc0ee64d81a93b
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-08-29 01:40:00 -0400

    same view as the one used by rest_auth only difference is the serializer used

---
+++ src/bloggit_project/utils/views.py
@@ -0,0 +1,30 @@
+from django.utils.translation import ugettext_lazy as _
+
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+
+from bloggit_project.utils.serializers import CustomPasswordResetSerializer
+
+class CustomPasswordResetView(GenericAPIView):
+    """
+    Calls Django Auth PasswordResetForm save method.
+
+    Accepts the following POST parameters: email
+    Returns the success/fail message.
+    """
+    serializer_class = CustomPasswordResetSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        # Create a serializer with request.data
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+        # Return the success message with OK HTTP status
+        return Response(
+            {"detail": _("Password reset e-mail has been sent.")},
+            status=status.HTTP_200_OK
+        )
