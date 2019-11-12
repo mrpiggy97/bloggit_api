46f021a0d8d851a48ec5b3f64e7ddad0f66ad678
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-11-11 23:18:30 -0500

    added more logic

--- src/posts_app/views/CommentView.py
+++ src/posts_app/views/CommentView.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 
-from rest_framework.views import View
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -13,7 +13,7 @@ from users_app.models import Sub
 from bloggit_project.utils.authentication import CustomJSONWebTokenAuthentication
 from bloggit_project.utils.permissions import ReadOrOwnerOnly
 
-class CommentView(View):
+class CommentView(APIView):
     
     original_serializer = OriginalCommentSerializer
     child_serializer = ChildCommentSerializer
@@ -42,22 +42,29 @@ class CommentView(View):
         else:
             return None
     
+    def get_invalid_message(self):
+        return 'invalid data'
+    
     def get(self, request, *args, **kwargs):
         
         comment = self.get_object()
         context = self.get_serializer_context()
         
         if comment.is_original:
-            serializer = OriginalCommentSerializer
+            serializer = OriginalCommentSerializer(comment, context=context)
         else:
-            serializer = ChildCommentSerializer
+            serializer = ChildCommentSerializer(comment, context=context)
         
         status_code = status.HTTP_200_OK
-        return Response(data=serializer.data, status=status_code)
+        data = {
+            'authenticated': request.user.is_authenticated,
+            'comment': serializer.data
+        }
+        return Response(data=data, status=status_code)
 
     def post(self, request, *args, **kwargs):
         
-        data = request.POST.get('data')
+        data = request.data
         context = self.get_serializer_context()
         
         if kwargs.get('is_original') == True:
@@ -66,12 +73,31 @@ class CommentView(View):
             serializer = ChildCommentSerializer(data=data, context=context)
         
         if serializer.is_valid():
-            serializer.save()
+            
+            if kwargs.get('is_original') == True:
+                if data.get('post_uuid') == None or data.get('owner_uuid') == None:
+                    status_code = status.HTTP_400_BAD_REQUEST
+                    data = {
+                        'message': 'you must provide post_uuid and owner_uuid'
+                    }
+                    return Response(data=data, status=status_code)
+            elif kwargs.get('is_original') == False:
+                if data.get('commentfeed_uuid') == None or data.get('owner_uuid') == None:
+                    status_code = status.HTTP_400_BAD_REQUEST
+                    data = {
+                        'message': 'you must provide commentfeed_uuid and owner_uuid'
+                    }
+                    return Response(data=data, status=status_code)
+                
             status_code = status.HTTP_201_CREATED
+            serializer.save()
             return Response(data=serializer.data, status=status_code)
         else:
             status_code = status.HTTP_400_BAD_REQUEST
-            return Response(data=None, status=status_code)
+            data = {
+                'message': self.get_invalid_message()
+            }
+            return Response(data=data, status=status_code)
     
     def put(self, request, *args, **kwargs):
         
@@ -84,12 +110,32 @@ class CommentView(View):
             serializer = ChildCommentSerializer(comment, data=data)
         
         if serializer.is_valid():
+            
+            if kwargs.get('is_original') == True:
+                if data.get('post_uuid') != None or data.get('owner_uuid') != None:
+                    data = {
+                        'message': 'post_uuid and owner_uuid must be None'
+                    }
+                    status_code = status.HTTP_400_BAD_REQUEST
+                    return Response(data=data, status=status_code)
+                
+            elif kwargs.get('is_original') == False:
+                if data.get('commentfeed_uuid') != None or data.get('owner_uuid') != None:
+                    data = {
+                        'message': 'commentfeed_uuid and owner_uuid must be None'
+                    }
+                    status_code = status.HTTP_400_BAD_REQUEST
+                    return Response(data=data, status=status_code)
+                
             serializer.save()
             status_code = status.HTTP_200_OK
             return Response(data=serializer.data, status=status_code)
         else:
             status_code = status.HTTP_400_BAD_REQUEST
-            return Response(data=None, status=status_code)
+            data = {
+                'message': self.get_invalid_message()
+            }
+            return Response(data=data, status=status_code)
     
     def delete(self, request, *args, **kwargs):
         
