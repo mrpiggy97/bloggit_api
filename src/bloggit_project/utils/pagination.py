bc33736566d0903691a13a37387d24170a782b43
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-11-22 21:44:07 -0500

    addde request as paramter

--- src/bloggit_project/utils/pagination.py
+++ src/bloggit_project/utils/pagination.py
@@ -2,12 +2,13 @@ from rest_framework.pagination import PageNumberPagination
 
 class CustomPagination(PageNumberPagination):
     #to make things easier
-    def get_paginated_data(self, data):
+    def get_paginated_data(self, data, request):
         paginated_data = {
             'count': self.page.paginator.count,
             'next': self.get_next_link(),
             'previous': self.get_previous_link(),
-            'results': data
+            'results': data,
+            'authenticated': request.user.is_authenticated
         }
 
         return paginated_data
