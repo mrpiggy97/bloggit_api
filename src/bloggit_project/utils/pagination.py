d12e6cce73c1fcba830f9443bb37a0b1b26ba588
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-01-03 02:16:19 -0500

    replaced links for page numbers in next and previous values

--- src/bloggit_project/utils/pagination.py
+++ src/bloggit_project/utils/pagination.py
@@ -3,10 +3,20 @@ from rest_framework.pagination import PageNumberPagination
 class CustomPagination(PageNumberPagination):
     #to make things easier
     def get_paginated_data(self, data, request):
+        if self.page.has_next():
+            next_page = self.page.next_page_number()
+        else:
+            next_page = None
+        
+        if self.page.has_previous():
+            previous_page = self.page.previous_page_number()
+        else:
+            previous_page = None
+            
         paginated_data = {
             'count': self.page.paginator.count,
-            'next': self.get_next_link(),
-            'previous': self.get_previous_link(),
+            'next': next_page,
+            'previous': previous_page,
             'results': data,
             'authenticated': request.user.is_authenticated
         }
