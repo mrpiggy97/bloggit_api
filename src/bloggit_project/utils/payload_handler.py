afc8043a96cd19f7f421f07ea9412064787e970f
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-01-15 10:58:45 -0500

    added profile_pic and communities to payload_handler

--- src/bloggit_project/utils/payload_handler.py
+++ src/bloggit_project/utils/payload_handler.py
@@ -1,6 +1,12 @@
+from users_app.models import Sub
+
 
 def payload_handler(token, user=None, request=None):
+    
+    sub = Sub.objects.get(user=user)
     return {
         'token': token,
         'username': user.username,
+        'profile_pic': sub.get_profile_pic_url,
+        'communities': sub.get_communities_as_list
     }
