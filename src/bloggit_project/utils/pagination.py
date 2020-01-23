1fe0d9602c101c670a6f2974bfd61b60d5eb93d1
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-01-23 01:26:26 -0500

    replaced if else block and changed it for if statements

--- src/bloggit_project/utils/pagination.py
+++ src/bloggit_project/utils/pagination.py
@@ -3,15 +3,14 @@ from rest_framework.pagination import PageNumberPagination
 class CustomPagination(PageNumberPagination):
     #to make things easier
     def get_paginated_data(self, data, request):
+        next_page = None
+        previous_page = None
+        
         if self.page.has_next():
             next_page = self.page.next_page_number()
-        else:
-            next_page = None
         
         if self.page.has_previous():
             previous_page = self.page.previous_page_number()
-        else:
-            previous_page = None
             
         paginated_data = {
             'count': self.page.paginator.count,
