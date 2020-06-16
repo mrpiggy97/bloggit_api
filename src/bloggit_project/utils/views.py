a03ba1e91be1cc8dca651846a2be972656df0399
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-06-15 20:48:11 -0400

    method was overriden, made the necessary imports

--- src/bloggit_project/utils/views.py
+++ src/bloggit_project/utils/views.py
@@ -1,11 +1,14 @@
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
-
+from rest_auth.app_settings import (TokenSerializer, JWTSerializer)
 from rest_auth.registration.views import RegisterView
+
+from allauth.account import app_settings as allauth_settings
 
 from users_app.models import Sub
 
@@ -38,13 +41,33 @@ class CustomRegisterView(RegisterView):
     '''create sub along with user'''
     #method overriden so a sub can be created when a user is created
     #nothing is deleted or changed, only Sub.object.create was inserted
+    def get_response_data(self, user):
+        if allauth_settings.EMAIL_VERIFICATION == \
+                allauth_settings.EmailVerificationMethod.MANDATORY:
+            return {"detail": _("Verification e-mail sent.")}
+
+        if getattr(settings, 'REST_USE_JWT', False):
+            #overrode method to return what custom payload handler returns
+            data = {
+                'username': user.username,
+                'token': self.token
+            }
+            return data
+        else:
+            return TokenSerializer(user.auth_token).data
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = self.perform_create(serializer)
-        Sub.objects.create(user=user)
+        sub = Sub.objects.create(user=user)
         headers = self.get_success_headers(serializer.data)
 
-        return Response(self.get_response_data(user),
+        response = self.get_response_data(user)
+        response['authenticated'] = True
+        response['communities'] = sub.get_communities_as_list
+        response['profile_pic'] = sub.get_profile_pic_url
+
+
+        return Response(response,
                         status=status.HTTP_201_CREATED,
                         headers=headers)
