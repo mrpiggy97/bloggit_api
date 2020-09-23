0ef2cfecbceee3130a5816208deb3886c3929b7c
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-09-22 20:34:54 -0400

    changed variable name from sub to session_sub and deleted request parameter for payload_handler function

--- src/bloggit_project/utils/payload_handler.py
+++ src/bloggit_project/utils/payload_handler.py
@@ -1,13 +1,16 @@
 from users_app.models import Sub
 
 
-def payload_handler(token, user=None, request=None):
-    
-    sub = Sub.objects.get(user=user)
+def payload_handler(token, user=None, request=None, session_sub=None):
+    #user should never be None
+    if user is None:
+        raise ValueError("user is not supposed to be none")
+    if session_sub is None:
+        session_sub = Sub.objects.get(user=user)
     return {
         'token': token,
         'username': user.username,
-        'profile_pic': sub.get_profile_pic_url,
-        'communities': sub.get_communities_as_list,
+        'profile_pic': session_sub.get_profile_pic_url,
+        'communities': session_sub.get_communities_as_list,
         'authenticated' : True
     }
